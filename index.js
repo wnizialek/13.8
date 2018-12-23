1a4980254c5bd636482ae21b26db1821f5650ba9
Wojciech Nizialek <wojciech.nizialek@gmail.com>
2018-12-23 20:08:21 +0000

    Change code

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ var fileData = fs.readFile('./index.html', 'utf-8', function(err, data) {
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/hello') {
-        response.write(fileData);
+        response.write("fileData");
             response.end();
     } else {
             response.statusCode = 404;
-            response.write('https://cdn.shopify.com/s/files/1/0322/6897/files/404-permalink_1170x1170.png?432866230176278629');
+            response.write('404');
             response.end();
     }
 });
